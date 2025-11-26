04d32290bf5dd688d32dc665d96a84f948c3c89a
Ryan <betrix303@gmail.com>
2025-11-25 21:49:12 -0500

    add react compiler

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,10 +5,18 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), tanstackRouter({}), react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({}),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
